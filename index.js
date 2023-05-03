5a36093f08a66822d2bcc697def22a38219707d5
allonsycrys <cristynabcosta@gmail.com>
2023-05-03 10:22:48 -0300

    Updates

--- index.js
+++ index.js
@@ -15,10 +15,11 @@ cardForm.addEventListener("submit", function (event) {
 })
 
 /*adiciona um event listener do tipo submit, ativando a função com o parâmetro evento, 
-depois, event.preventDefault() faz com que o formulário não seja enviado automáticamente 
+depois, event.preventDefault() faz com que o formulário não seja enviado automaticamente 
 quando a página carrega 
 */
-let cardCount = 1
+
+let cardCount = 1 //define a variável que vai armazenar o contador de cartões para o cartão ir para a posiçõa certa na carteira
 
 function checkCard(cardNumber) {
   const cardNumberValidate = validator.isValid(cardNumber)
@@ -29,24 +30,35 @@ function checkCard(cardNumber) {
 
     if (cardCount === 1) {
       document.getElementById('walletCardOne').innerHTML = validator.maskify(cardNumber)
+      const walletTextOne = document.getElementById('walletCardOne');
+      walletTextOne.style.color = "black"
     } else if (cardCount === 2) {
       document.getElementById('walletCardTwo').innerHTML = validator.maskify(cardNumber)
+      const walletTextTwo = document.getElementById('walletCardTwo');
+      walletTextTwo.style.color = "black"
     } else if (cardCount === 3) {
       document.getElementById('walletCardThree').innerHTML = validator.maskify(cardNumber)
+      const walletTextThree = document.getElementById('walletCardThree');
+      walletTextThree.style.color = "black"
     } else if (cardCount === 4) {
       document.getElementById('walletCardFour').innerHTML = validator.maskify(cardNumber)
+      const walletTextFour = document.getElementById('walletCardFour');
+      walletTextFour.style.color = "black"
     } else if (cardCount === 5) {
       document.getElementById('walletCardFive').innerHTML = validator.maskify(cardNumber)
+      const walletTextFive = document.getElementById('walletCardFive');
+      walletTextFive.style.color = "black"
     } else if (cardCount >= 6) {
       alert("Sua carteira está cheia")
     }
     cardCount++
   } else if (cardNumberValidate === false) {
     alert('Número de cartão inválido')
-  }
+  } //criar os elementos com o javascript create element
 }
 
-
+// cada if(no caso de cartão válido pelo isValid()): seleciona o lugar certo na carteira com getElement --> substitui o conteúdo pelo valor do cartão mascarado com o maskify --> substitui a cor do texto na carteira por preto
+// 
 
 
 
--- validator.js
+++ validator.js
@@ -1,53 +1,53 @@
 const validator = {
   isValid: function (cardNumber) {
 
-    const cardNumberInput = cardNumber.split('').reverse()
+    const cardNumberInput = cardNumber.split('').reverse() // transforma o input em uma array com split e inverte ela com o reverse
+    
 
-
-    function getEvenIndex(item, index) {
+    function getEvenIndex(item, index) { //pega os números em posição par
       return index % 2 === 0
     }
     const cardNumberEvens = cardNumberInput.filter(getOddIndex)
 
-    function getOddIndex(item, index) {
+    function getOddIndex(item, index) { // pega os números em posição ímpar
       return index % 2 !== 0
     }
 
     const cardNumberOdds = cardNumberInput.filter(getEvenIndex)
 
-    const cardNumberDoubledEvens = cardNumberEvens.map(x => x * 2)
+    const cardNumberDoubledEvens = cardNumberEvens.map(x => x * 2) // dobra os pares
 
-    function getTwoDigits(item) {
+    function getTwoDigits(item) {                         //seliona os com 2 dígitos
       return item > 9
     }
 
-    function getOneDigits(item) {
+    function getOneDigits(item) {                        //seliona os com 1 dígito
       return item <= 9
     }
 
-    const cardNumbersEvensOneDigits = cardNumberDoubledEvens.filter(getOneDigits)
+    const cardNumbersEvensOneDigits = cardNumberDoubledEvens.filter(getOneDigits) // cria a array com 1 dígito
 
-    const cardNumbersEvensTwoDigits = cardNumberDoubledEvens.filter(getTwoDigits)
+    const cardNumbersEvensTwoDigits = cardNumberDoubledEvens.filter(getTwoDigits) // cria a array com 2 dígitos
 
-    const cardNumberNewOneDigits = cardNumbersEvensTwoDigits.map(function (item) {
+    const cardNumberNewOneDigits = cardNumbersEvensTwoDigits.map(function (item) { // divide os double digits em uma array de dois dígitos separados ['1', '2']
       return item.toString(10).split('')
     })
 
-    const cardNumbersNewOneDigitsSum = cardNumberNewOneDigits.map(function (item) {
+    const cardNumbersNewOneDigitsSum = cardNumberNewOneDigits.map(function (item) { // soma os dígitos
       return parseInt(item[0]) + parseInt(item[1])
     })
 
 
-    const cardNumberFinal = cardNumberOdds.concat(cardNumbersEvensOneDigits, cardNumbersNewOneDigitsSum)
+    const cardNumberFinal = cardNumberOdds.concat(cardNumbersEvensOneDigits, cardNumbersNewOneDigitsSum) // concatena todas as arrays
 
-    const cardNumberFinalInts = cardNumberFinal.map(function (item) {
+    const cardNumberFinalInts = cardNumberFinal.map(function (item) { //transforma em integer
       return parseInt(item)
     })
 
 
-    const sumOfCardNumberDigits = cardNumberFinalInts.reduce((sum, a) => sum + a, 0)
+    const sumOfCardNumberDigits = cardNumberFinalInts.reduce((sum, a) => sum + a, 0) //soma todos os números da array com o método reduce
 
-    if (sumOfCardNumberDigits % 10 === 0) {
+    if (sumOfCardNumberDigits % 10 === 0) {       //determina o retorno com base no algoritmo
       return true
     } else {
       return false
@@ -56,15 +56,15 @@ const validator = {
   ,
   maskify: function (cardNumber) {
 
-    const unmaskedDigits = cardNumber.slice(-4).split('')
+    const unmaskedDigits = cardNumber.slice(-4).split('')  //faz uma array com os números que não precisam ser mascarados
     const maskedDigits = []
 
-    for (let i = 0; i < cardNumber.length - 4; i++) {
+    for (let i = 0; i < cardNumber.length - 4; i++) {       //faz uma array com os últimos 4 números mascarados
       maskedDigits.push("#")
     }
 
-    const maskedCardNumber = maskedDigits.concat(unmaskedDigits)
-    return maskedCardNumber.join('')
+    const maskedCardNumber = maskedDigits.concat(unmaskedDigits)    //concatena as arrays
+    return maskedCardNumber.join('')                                //transforma em string //replace
 
 
 
